cb202bfc4a984970a1c6884d35c2e9cd9503b215
chirag shukla <shuklachirag57@gmail.com>
2024-04-20 11:31:13 +0530

    firstcommit

---
+++ day1/day1.js
@@ -0,0 +1,9 @@
+document.write("hey")
+//alert("error")//to give an pop up msg like invalid on screen
+var a = 10
+var a = 20
+let n = 10
+//can be declared multiple times 
+n = 20
+//can be reassigned/overwrite in var
+document.write(a)//to print
---
+++ day2/day2.js
@@ -0,0 +1,21 @@
+console.log("hello world")
+document.write("hello world doucment")
+// js supports all tags of html ex
+document.write("<h1>hello world</h1>")
+//alert("error") to get an pop up box like invalid
+//variable are used to store data and to perform operations variables are contianers
+let a = 10
+var b = 90
+const = 13
+//these are keywords not data types
+document.write("hello",a)
+//let
+//let is local scope and var is global scope
+//let can be only reassigned
+
+//const
+const  nm = "chirag"
+nm ="shukla"
+document.write(nm) 
+//cant be  reassigned and redeclared 
+//it is local scope
